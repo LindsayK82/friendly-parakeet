8442a6f492c0ec08c5ff26c16afae4bf721ab2af
Lindsay <lindsayklug20@yahoo.com>
2022-10-27 19:33:15 -0400

    JS update

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,39 @@
-// Assignment code here
+// // Assignment code here
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+var generate = document.querySelector("#generate")
+var password = document.getElementById("password");
+var chars = ["0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*ABCDEFGHIJKL"]
+var passwordLength = 12;
+var password = "";
+
+function writePassword( {
+  var generate = window.prompt();
+
+  for (var i = 0; i <= passwordLength; i++;) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+
+  if (!userChoice) {
+    return;
+  }
+
+  var password = window.prompt("");
+};
+

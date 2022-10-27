1b0fd5aac3af6303548a16454afd89a8cf2445a6
Lindsay <lindsayklug20@yahoo.com>
2022-10-27 19:59:26 -0400

    Update to README and JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,24 @@
-// // Assignment code here
-
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Created variables for the needed elements on the page.
 
 var generate = document.querySelector("#generate")
 var password = document.getElementById("password");
 var chars = ["0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*ABCDEFGHIJKL"]
 var passwordLength = 12;
 var password = "";
-
-function writePassword( {
-  var generate = window.prompt();
-
-  for (var i = 0; i <= passwordLength; i++;) {
+// created a function to generate the password. On loading the page, the prompt shows up.
+function writePassword() {
+  var generate = window.prompt("Please press OK to confirm request");
+// integer function for random generation using several variables created above.
+  for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber +1);
   }
-
+// if the user cancels, the page resets
   if (!userChoice) {
     return;
   }
-
+// this should create the password prompt but is not functioning.
   var password = window.prompt("");
 };
-
+// This code launches the function to the site.
+writePassword();
